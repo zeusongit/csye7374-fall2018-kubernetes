3a713df13682d11ab6911e0e9e3badc2608ca265
Aditya Kelkar <kelkar.adi@northeastern.edu>
2018-10-13 12:33:50 -0400

    Templates and startup script updates

--- redis/setup.sh
+++ redis/setup.sh
@@ -5,3 +5,6 @@ kubectl create -f templates/redis-sentinel-service.yaml
 kubectl create -f templates/redis-master.yaml
 kubectl create -f templates/redis-controller.yaml
 kubectl create -f templates/redis-sentinel-controller.yaml
+
+kubectl scale rs redis --replicas=3
+kubectl scale rs redis-sentinel --replicas=3
