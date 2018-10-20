bd750fc7f21a3196f99f0d77dcbcc4a92d505c27
Aditya Kelkar <kelkar.adi@northeastern.edu>
2018-10-19 23:17:49 -0400

    Templates, readme updated

--- redis/setup.sh
+++ redis/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-kubectl create -f templates/nodeport-service.yaml
+kubectl create -f templates/master-replica.yaml
+kubectl create -f templates/master-service.yaml
 kubectl create -f templates/redis-sentinel-service.yaml
-kubectl create -f templates/redis-master.yaml
-kubectl create -f templates/redis-controller.yaml
-kubectl create -f templates/redis-sentinel-controller.yaml
-
-kubectl scale rs redis --replicas=3
-kubectl scale rs redis-sentinel --replicas=3
+kubectl create -f templates/nodeport-service.yaml
+kubectl create -f templates/slave-replica.yaml
+kubectl create -f templates/slave-service.yaml
