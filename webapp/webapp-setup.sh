1c1d00b5bf462a65107b8c47b4b25b46415cdd90
Aditya Kelkar <kelkar.adi@northeastern.edu>
2018-10-26 18:33:38 -0400

    Folder restructure, redis and webapp config updates

--- webapp/webapp-setup.sh
+++ webapp/webapp-setup.sh
@@ -5,3 +5,7 @@ kubectl create configmap redishost --from-literal=REDISHOST=${REDISHOST_IP}
 kubectl create configmap csye7374-port --from-literal=port=9090	
 kubectl get configmaps redishost -o yaml
 
+
+kubectl apply -f csye-app.yaml
+kubectl apply -f csye-service.yaml
+
