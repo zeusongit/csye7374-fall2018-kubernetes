df95bead03504edb85acaeb3399426b62b92d314
Ashish <aggarwal.ash@husky.neu.edu>
2018-11-03 10:52:06 -0700

    a41

--- assignment3/appsetup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f csye-configmap.yaml
-kubectl apply -f redis-master.yaml
-sh redisconnect.sh
-kubectl apply -f csye-app.yaml
-kubectl apply -f csye-service.yaml
--- assignment3/appteardown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f csye-configmap.yaml
-kubectl delete -f redis-master.yaml
-kubectl delete -f csye-app.yaml
-kubectl delete -f csye-service.yaml
