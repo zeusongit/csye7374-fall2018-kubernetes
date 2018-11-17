ced913187726aa1e2f84e3d6b9f5eeefb14bc602
Nathaly <lozano.n@husky.neu.edu>
2018-11-17 13:14:34 -0500

    Updating K8 setup

--- k8/k8teardown.sh
+++ k8/k8teardown.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export NAME=csye6225-spring2018-aggarwalash.me
-export KOPS_STATE_STORE=s3://csye6225-spring2018-aggarwalash.me
-
+export NAME=lozano.k8s.tejasparikh.com
+export KOPS_STATE_STORE=s3://lozano.k8s.tejasparikh.com
 kops delete cluster --name $NAME --yes
